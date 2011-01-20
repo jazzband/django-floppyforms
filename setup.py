0aa21a2a6a61754796a5b58668155a2596beb0e1
Bruno Renié <buburno@gmail.com>
2011-01-20 09:27:59 +0100

    Version bump: releasing 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='django-floppyforms',
-    version='0.1',
+    version='0.2',
     author=u'Bruno Renie',
     author_email='bruno@renie.fr',
     packages=find_packages(),
