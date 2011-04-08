f6a74d638431a193358b6b604a2b03c468a52138
Bruno Renié <buburno@gmail.com>
2011-04-08 10:28:34 +0200

    0.4.4 on PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='django-floppyforms',
-    version='0.4.3',
+    version='0.4.4',
     author=u'Bruno Renie',
     author_email='bruno@renie.fr',
     packages=find_packages(),
