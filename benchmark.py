02d98c0cd93ba0b998de278649e75f4f3e2bc2d3
Bruno Renié <buburno@gmail.com>
2012-01-02 14:47:55 +0100

    Updated benchmark.py for easier use

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,9 @@
 """
 Compares the rendering speed between Django forms and django-floppyforms
+
+Usage: DJANGO_SETTINGS_MODULE=benchmark python benchmark.py [--cache]
 """
+import sys
 import timeit
 
 django = """from django import forms
@@ -31,5 +34,18 @@ def time(stmt):
     t = timeit.Timer(stmt=stmt)
     return t.timeit(number=1000)
 
-print "Plain Django:", time(django)
-print "Django-floppyforms:", time(flop)
+if __name__ == '__main__':
+    print "Plain Django:", time(django)
+    print "Django-floppyforms:", time(flop)
+
+INSTALLED_APPS = (
+    'floppyforms'
+)
+
+if '--cache' in sys.argv:
+    TEMPLATE_LOADERS = (
+        ('django.template.loaders.cached.Loader', (
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        )),
+    )
