89c7d7179c8f02005e2d1967ebcbfe78780dc7e9
Jason Michalski <jmichalski@ruelala.com>
2012-08-20 15:40:39 -0400

    IntegerField should not inherit from FloatField

---
+++ floppyforms/tests/fields.py
@@ -0,0 +1,10 @@
+from .base import FloppyFormsTestCase
+
+import floppyforms as forms
+
+class IntegerFieldTests(FloppyFormsTestCase):
+    def test_parse_int(self):
+        int_field = forms.IntegerField()
+        result = int_field.clean('15')
+        self.assertTrue(15, result)
+        self.assertIsInstance(result, int)
--- floppyforms/tests/tests.py
+++ floppyforms/tests/tests.py
@@ -5,3 +5,4 @@ from .layouts import *
 from .rendering import *
 from .templatetags import *
 from .widgets import *
+from .fields import *
