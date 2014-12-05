e78a32f58f88166cccc693fc7e0c744ac498c606
Gregor Müllegger <gregor@muellegger.de>
2014-12-05 12:24:28 +0100

    Adding tests for DateTimeField in order to test for issue report in #132.

--- tests/fields.py
+++ tests/fields.py
@@ -1,6 +1,25 @@
+from datetime import datetime
 from django.test import TestCase
 
 import floppyforms as forms
+
+
+class DateTimeFieldTests(TestCase):
+    def test_parse_datetime(self):
+        field = forms.DateTimeField()
+        result = field.clean('2000-01-01')
+        self.assertEqual(result, datetime(2000, 1, 1))
+
+    def test_data_is_being_parsed(self):
+        class SampleForm(forms.Form):
+            datetime_field = forms.DateTimeField()
+
+        form = SampleForm({'datetime_field': '2099-12-31'})
+        form.full_clean()
+        self.assertTrue(form.is_valid())
+        self.assertEqual(
+            form.cleaned_data['datetime_field'],
+            datetime(2099, 12, 31))
 
 
 class IntegerFieldTests(TestCase):
