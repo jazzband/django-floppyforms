6911d97ac4552f037a00e5aae96853e9a058b464
Yuki Izumi <yuki@kivikakk.ee>
2015-02-06 20:25:52 +1100

    Don't crash in bdist_rpm with unicode issues.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def find_version(*file_paths):
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
                               version_file, re.M)
     if version_match:
-        return version_match.group(1)
+        return str(version_match.group(1))
     raise RuntimeError("Unable to find version string.")
 
 
