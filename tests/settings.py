e8bc9b6d46e59fa9197332c3effa59bb4cce4741
Gregor Müllegger <gregor@muellegger.de>
2015-04-20 09:34:05 +0200

    Defining TEMPLATES settings for test to prevent new deprecation warning.

--- tests/settings.py
+++ tests/settings.py
@@ -23,6 +23,26 @@ STATIC_URL = '/static/'
 
 SECRET_KEY = '0'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+            ],
+        },
+    },
+]
+
+
 import django
 if django.VERSION < (1, 6):
     TEST_RUNNER = 'discover_runner.DiscoverRunner'
