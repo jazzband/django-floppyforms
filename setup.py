ee563af6f43be85aca129e041ff87af85d133a9c
Gregor Müllegger <gregor@muellegger.de>
2015-09-28 13:18:42 +0200

    Add Django 1.9 trove classifier

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         'Framework :: Django :: 1.6',
         'Framework :: Django :: 1.7',
         'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
